9739f5ad7fc94da7f412a22c213209c21d745589
Sophievcy <sophievc@shaw.ca>
2025-10-28 01:18:23 -0700

    added panel plots for other model prediction methods

--- scripts/8. panel_plots.R
+++ scripts/8. panel_plots.R
@@ -16,6 +16,30 @@ plot_grid(
   nrow = 2
 )
 
+#PROPORTION VIGILANCE - combined plots, averaged predictions
+plot_grid(
+  combined_avg_vigilance_year_plot,
+  combined_avg_vigilance_predator_plot,
+  combined_avg_vigilance_habitat_plot,
+  combined_avg_vigilance_prey_plot,
+  labels = c("A", "B", "C", "D"), 
+  label_size = 14,  
+  ncol = 2,
+  nrow = 2
+)
+
+#PROPORTION VIGILANCE - combined plots, fixed predictions
+plot_grid(
+  combined_fixed_vigilance_year_plot,
+  combined_fixed_vigilance_predator_plot,
+  combined_fixed_vigilance_habitat_plot,
+  combined_fixed_vigilance_prey_plot,
+  labels = c("A", "B", "C", "D"), 
+  label_size = 14,  
+  ncol = 2,
+  nrow = 2
+)
+
 #LATENCY TO FLEE - Kaplan-mier curves 
 plot_grid(
   year_plot$plot,
@@ -34,6 +58,30 @@ plot_grid(
   combined_frequency_pred_plot,
   combined_frequency_habitat_plot,
   combined_frequency_prey_plot,
+  labels = c("A", "B", "C", "D"), 
+  label_size = 14,  
+  ncol = 2,
+  nrow = 2
+)
+
+#FREQUENCY OF FLIGHT - combined plots, averaged predictions
+plot_grid(
+  combined_avg_frequency_year_plot,
+  combined_avg_frequency_pred_plot,
+  combined_avg_frequency_habitat_plot,
+  combined_avg_frequency_prey_plot,
+  labels = c("A", "B", "C", "D"), 
+  label_size = 14,  
+  ncol = 2,
+  nrow = 2
+)
+
+#FREQUENCY OF FLIGHT - combined plots, fixed predictions
+plot_grid(
+  combined_fixed_frequency_year_plot,
+  combined_fixed_frequency_pred_plot,
+  combined_fixed_frequency_habitat_plot,
+  combined_fixed_frequency_prey_plot,
   labels = c("A", "B", "C", "D"), 
   label_size = 14,  
   ncol = 2,
