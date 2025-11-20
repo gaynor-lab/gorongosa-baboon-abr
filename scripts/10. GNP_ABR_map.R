8c772f2afbd3a18915eb800ba2092d33050cacae
Sophievcy <sophievc@shaw.ca>
2025-11-19 19:02:47 -0800

    updated map for gorongosa/africa

--- scripts/10. GNP_ABR_map.R
+++ scripts/10. GNP_ABR_map.R
@@ -9,8 +9,8 @@ library(ggplot2)
 library(rnaturalearth)
 library(rnaturalearthdata)
 library(wdpar)
+library(ggspatial)
 
-install.packages("geojsonio")
 #read in data here
 ABR_data <- read.csv("data/ABR_locations.csv")
 
@@ -68,10 +68,9 @@ leaflet() %>%
     position = "topright"
   )
 
-#Map 2 - with shapefiles
+#Map of camera trap sites - with shapefiles
 
 #install packages
-install.packages(c("sf", "ggplot2", "dplyr"))
 library(sf)
 library(ggplot2)
 library(dplyr)
@@ -80,16 +79,22 @@ library(dplyr)
 #Floodplain <- st_read("data/GNP_shp/Floodplain_only.shp")
 GNP_boundary <- st_read("data/GNP_shp/GNP_Landscapes_New_boundary.shp")
 Rivers <- st_read("data/GNP_shp/gp_permanent_rivers_utm.shp")
-#Grasslands <- st_read("data/GNP_shp/Grassland_only.shp")
+Grasslands <- st_read("data/GNP_shp/Grassland_only.shp")
 Lake_urema <- st_read("data/GNP_shp/LakeUrema.shp")
 Roads <- st_read("data/GNP_shp/roads_may2015.shp")
+Africa <- st_read("data/GNP_shp/ne_10m_admin_0_countries.shp") %>%
+  filter(REGION_UN == "Africa")
 
-#visualize - delete later
-plot(st_geometry(Floodplain))
-plot(st_geometry(GNP_boundary))
-plot(st_geometry(Rivers))
-plot(st_geometry(Grasslands))
-plot(st_geometry(Lake_urema))
+#load ABR data
+ABR_data <- read.csv("data/ABR_locations.csv")
+
+# Convert to sf object 
+ABR_data <- st_as_sf(
+  ABR_data,
+  coords = c("Long", "Lat"),   
+  crs = 4326                   
+)
+bbox <- st_bbox(ABR_data)
 
 #Match projections
 # Use GNP_boundary as reference CRS
@@ -97,38 +102,124 @@ crs_ref <- st_crs(GNP_boundary)
 
 Floodplain  <- st_transform(Floodplain, crs_ref)
 Rivers      <- st_transform(Rivers, crs_ref)
-st_crs(Grasslands) <- st_crs(GNP_boundary)
+st_crs(Grasslands) <- 4326
 Lake_urema  <- st_transform(Lake_urema, crs_ref)
 Roads       <- st_transform(Roads, crs_ref)
+ABR_data <- st_transform(ABR_data, st_crs(GNP_boundary)) 
 
 
-#plotting first attempt - CRS is off
+#plot
 ggplot() +
   
-  # Landscape boundary
-  geom_sf(data = GNP_boundary, fill = NA, color = "black", linewidth = 0.6) +
-  
-  # Floodplain
-  geom_sf(data = Floodplain, fill = "lightblue", color = NA, alpha = 0.5) +
-  
-  # Grasslands
-  geom_sf(data = Grasslands, fill = "tan", color = NA, alpha = 0.5) +
+  # Park background
+  geom_sf(data = GNP_boundary, fill = "#A3B18A", color = "black") +
   
   # Lake Urema
-  geom_sf(data = Lake_urema, fill = "blue", color = NA, alpha = 0.7) +
-  
-  # Rivers
-  geom_sf(data = Rivers, color = "steelblue", linewidth = 0.4) +
+  geom_sf(data = Lake_urema, fill = "steelblue3", alpha = 0.6) +
   
   # Roads
-  geom_sf(data = Roads, color = "black", linewidth = 0.6) +
+  geom_sf(data = Roads, color = "grey40", size = 0.3) +
   
-  # Theme
+  # Camera trap points
+  geom_sf(
+    data = ABR_data,
+    aes(color = Habitat, size = total),
+    alpha = 0.9
+  ) +
+  
+  # Zoom window (with buffer)
+  coord_sf(
+    xlim = c(bbox["xmin"] - 700, bbox["xmax"] + 700),
+    ylim = c(bbox["ymin"] - 700, bbox["ymax"] + 700),
+    expand = FALSE
+  ) +
+  
+  # Habitats
+  scale_color_manual(
+    values = c(
+      "Open" = "#0A0A52",   # Navy
+      "Closed" = "#A23B2A"  # Rusty red
+    ),
+    name = "Habitat"
+  ) +
+  
+  # Video count sizes
+  scale_size_continuous(
+    range = c(2, 10),
+    name = "Total Videos"
+  ) +
+  
+  # Scale bar
+  annotation_scale(
+    location = "bl",
+    width_hint = 0.3
+  ) +
+  
+  # North arrow
+  annotation_north_arrow(
+    location = "bl",
+    which_north = "true",
+    pad_x = unit(0.2, "in"),
+    pad_y = unit(0.2, "in"),
+    style = north_arrow_fancy_orienteering
+  ) +
+  
+  # Remove axes + grid
   theme_minimal() +
   theme(
+    axis.text = element_blank(),
+    axis.title = element_blank(),
+    axis.ticks = element_blank(),
     panel.grid = element_blank(),
-    axis.title = element_blank()
+    legend.position = "none"   # REMOVE legend
   )
 
+#Map of Goronogosa National Park in Mozambique
+Africa_utm <- st_transform(Africa, st_crs(GNP_boundary))
+moz <- Africa_utm %>%
+  filter(ADMIN == "Mozambique")
+st_crs(Africa)
+st_crs(GNP_boundary)
 
+
+ggplot() +
+  
+  # Africa in UTM
+  geom_sf(data = Africa_utm, fill = "#F0F0F0", color = "black") +
+  
+  #Mozambique in UTM
+  geom_sf(data = moz, fill = "#D9D9D9", color = "black") +
+  
+  # Gorongosa National Park
+  geom_sf(data = GNP_boundary, fill = "#556B2F", color = "black") +
+  
+  # Zoom to GNP
+  coord_sf(
+    xlim = c(bbox["xmin"] - 1000000, bbox["xmax"] + 1000000),
+    ylim = c(bbox["ymin"] - 1000000, bbox["ymax"] + 1000000),
+    expand = FALSE
+  ) +
+  
+  # Scale bar
+  annotation_scale(
+    location = "bl",
+    width_hint = 0.3
+  ) +
+  
+  # North arrow
+  annotation_north_arrow(
+    location = "bl",
+    which_north = "true",
+    pad_x = unit(0.2, "in"),
+    pad_y = unit(0.2, "in"),
+    style = north_arrow_fancy_orienteering
+  ) +
+
+  theme_minimal() +
+  theme(
+    axis.text = element_blank(),
+    axis.title = element_blank(),
+    axis.ticks = element_blank(),
+    panel.grid = element_blank()
+  )
 
