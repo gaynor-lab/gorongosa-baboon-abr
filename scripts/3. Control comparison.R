3b916055fda104d02f2c623653e1fb69dc176455
Kaitlyn Gaynor <gaynor@zoology.ubc.ca>
2026-06-16 15:40:45 -0700

    Calculate summary statistics for pred and control cues

--- scripts/3. Control comparison.R
+++ scripts/3. Control comparison.R
@@ -14,6 +14,28 @@ Baboon_flight_df <- readRDS("data_derived/Baboon_flight_binary_df.rds")
 
 # PROPORTION OF TIME SPENT VIGILANT-----
 
+
+# Summary stats
+
+#pred
+nrow(Baboon_vigilance_df %>% filter(proportion_vigilant == 0, cue_type == "Predator")) # 44 videos (9%) no vigilance
+nrow(Baboon_vigilance_df %>% filter(proportion_vigilant > 0, cue_type == "Predator")) # 431 videos (91%) vigilance
+
+#control
+nrow(Baboon_vigilance_df %>% filter(proportion_vigilant == 0, cue_type == "Control")) # 20 videos (22%) no vigilance
+nrow(Baboon_vigilance_df %>% filter(proportion_vigilant > 0, cue_type == "Control")) # 73 videos (78%) vigilance
+
+# mean and SD
+Baboon_vigilance_df %>%
+  filter(cue_type %in% c("Predator", "Control")) %>%
+  group_by(cue_type) %>%
+  summarise(
+    mean_proportion_vigilant = mean(proportion_vigilant, na.rm = TRUE),
+    sd_proportion_vigilant = sd(proportion_vigilant, na.rm = TRUE)
+  )
+
+# Model
+
 vigilance_comparison <- glmmTMB(proportion_vigilant_beta ~ cue_type + (1|site),
                                 data = Baboon_vigilance_df,
                                 family = beta_family(),
@@ -23,6 +45,18 @@ summary(vigilance_comparison)
 # P = 0.00014
 
 # FLIGHT-----
+
+# Summary stats
+
+#pred
+nrow(Baboon_flight_df %>% filter(flight_present == 0, cue_type == "Predator")) # 636 videos (82%) no flight
+nrow(Baboon_flight_df %>% filter(flight_present == 1, cue_type == "Predator")) # 140 videos (18%) flight
+
+#control
+nrow(Baboon_flight_df %>% filter(flight_present == 0, cue_type == "Control")) # 124 videos (90%) no flight
+nrow(Baboon_flight_df %>% filter(flight_present == 1, cue_type == "Control")) # 14 videos (10%) flight
+
+# Model
 
 flight_comparison <- glmmTMB(flight_present ~ cue_type + (1|site),
                              data = Baboon_flight_df,
